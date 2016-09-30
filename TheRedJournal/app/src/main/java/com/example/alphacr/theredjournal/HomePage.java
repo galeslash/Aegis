c66b1c45d08be32ee0ddd94667c00e9f605648d1
alphacr <ricky.arkan@ui.ac.id>
2016-09-30 18:18:40 +0700

    Added rough work
    Added rough work on home_page
    
    drawable and stuffs
    
    pictures
    
    Trivia implemented
    
    Cleaned up some codes
    
    Some Shits
    
    Trying to combine with master 2
    
    Added some button listener changes and strings
    
    Made Change Password UI

--- TheRedJournal/app/src/main/java/com/example/alphacr/theredjournal/HomePage.java
+++ TheRedJournal/app/src/main/java/com/example/alphacr/theredjournal/HomePage.java
@@ -1,5 +1,40 @@
 package com.example.alphacr.theredjournal;
 
+<<<<<<< HEAD
+<<<<<<< HEAD
+import android.content.Intent;
+=======
+=======
+
+>>>>>>> 19efcae... Trying to combine with master 2
+import android.support.annotation.IdRes;
+>>>>>>> 096dfb6... Trivia implemented
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.Button;
+<<<<<<< HEAD
+<<<<<<< HEAD
+import android.widget.EditText;
+
+import helper.SQLITEHandler;
+import helper.SessionManager;
+=======
+=======
+import android.widget.TextView;
+>>>>>>> 096dfb6... Trivia implemented
+import android.widget.Toast;
+<<<<<<< HEAD
+>>>>>>> 3c9ada3... drawable and stuffs
+
+public class HomePage extends AppCompatActivity {
+    EditText contactForm;
+    private Button btnLogOut;
+
+    private SQLITEHandler db;
+    private SessionManager session;
+=======
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,18 +44,27 @@ import android.widget.EditText;
 
 import helper.SQLITEHandler;
 import helper.SessionManager;
+>>>>>>> 19efcae... Trying to combine with master 2
 
 public class HomePage extends AppCompatActivity {
-    EditText contactForm;
-    private Button btnLogOut;
 
+    EditText contactForm;
     private SQLITEHandler db;
     private SessionManager session;
+    TextView factBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Facts factHolder;
+        Button button;
+        Button guide;
+        Button btnLogOut;
+        Button contactUs;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+<<<<<<< HEAD
         contactForm = (EditText) findViewById(R.id.contactUs);
 
         btnLogOut = (Button) findViewById(R.id.btnLogOut);
@@ -53,3 +97,71 @@ public class HomePage extends AppCompatActivity {
     finish();
     }
 }
+=======
+
+        // Fact Coding
+        factHolder = new Facts();
+        factBox = (TextView) findViewById(R.id.trivia_content);
+        factBox.setText(factHolder.nextFact());
+
+        // Button Listener
+        button = (Button) findViewById(R.id.btDonor);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Donor is pressed", Toast.LENGTH_SHORT).show();
+            }
+        });
+        guide = (Button) findViewById(R.id.btGuide);
+        guide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "How to Donor is pressed", Toast.LENGTH_SHORT).show();
+            }
+        });
+        btnLogOut = (Button) findViewById(R.id.btnLogOut);
+        db = new SQLITEHandler(getApplicationContext());
+        session = new SessionManager(getApplicationContext());
+        btnLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logOutUser();
+            }
+        });
+        contactUs = (Button) findViewById(R.id.contact);
+        contactUs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (HomePage.this, Contact_Us.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+
+    private void logOutUser(){
+        session.setLogin(false);
+        db.deleteUsers();
+        Intent intent = new Intent (HomePage.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+<<<<<<< HEAD
+    private  void logOutUser(){
+    session.setLogin(false);
+    db.deleteUsers();
+
+    Intent intent = new Intent (HomePage.this, LoginActivity.class);
+    startActivity(intent);
+    finish();
+    }
+<<<<<<< HEAD
+}
+>>>>>>> 3c9ada3... drawable and stuffs
+=======
+}
+>>>>>>> 19efcae... Trying to combine with master 2
+=======
+}
+>>>>>>> cdd4217... Added some button listener changes and strings
