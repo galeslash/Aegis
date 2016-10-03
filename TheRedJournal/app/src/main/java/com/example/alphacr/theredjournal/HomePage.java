3c9ada342eca44325bf272b797e8352b79df3c11
alphacr <ricky.arkan@ui.ac.id>
2016-10-03 10:42:27 +0700

    drawable and stuffs

--- TheRedJournal/app/src/main/java/com/example/alphacr/theredjournal/HomePage.java
+++ TheRedJournal/app/src/main/java/com/example/alphacr/theredjournal/HomePage.java
@@ -2,12 +2,30 @@ package com.example.alphacr.theredjournal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class HomePage extends AppCompatActivity {
 
+    private Button submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        submit = (Button) findViewById(R.id.button);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Submit is pressed", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+    // Bikin Array
+    // Random dapetin index array
+    // Assign textview
+
+    //set on click listener
 }
