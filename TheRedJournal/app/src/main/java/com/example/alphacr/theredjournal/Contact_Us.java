f72a975047b63ec6aefe082f1f95933c8d9977f9
Isaac Raynald <Isaac Raynald>
2016-10-05 17:23:31 +0700

    backend contact us 70%

--- TheRedJournal/app/src/main/java/com/example/alphacr/theredjournal/Contact_Us.java
+++ TheRedJournal/app/src/main/java/com/example/alphacr/theredjournal/Contact_Us.java
@@ -1,13 +1,27 @@
 package com.example.alphacr.theredjournal;
 
+import android.media.RemoteController;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
 
 public class Contact_Us extends AppCompatActivity {
-
+    EditText contact;
+    String str_contact;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact__us);
+        contact= (EditText)findViewById(R.id.contactUs);
+    }
+    public void onConfirm(View view){
+        str_contact = contact.getText().toString();
+        String type = "contact";
+        BackgroundWorker backgroundWorker = new BackgroundWorker(this);
+        backgroundWorker.execute(type, str_contact);
+
+
     }
 }
--- TheRedJournal/app/src/main/java/com/example/alphacr/theredjournal/HomePage.java
+++ TheRedJournal/app/src/main/java/com/example/alphacr/theredjournal/HomePage.java
@@ -1,13 +1,22 @@
 package com.example.alphacr.theredjournal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class HomePage extends AppCompatActivity {
+    EditText contactForm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        contactForm = (EditText) findViewById(R.id.contactUs);
+    }
+
+    public void contactUs(View view) {
+        startActivity(new Intent(this, Contact_Us.class));
     }
 }
