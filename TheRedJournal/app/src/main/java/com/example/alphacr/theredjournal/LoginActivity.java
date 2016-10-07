b6a04824651b6161bab53802c9b9b3d3a7604515
Isaac Raynald <Isaac Raynald>
2016-10-07 12:44:52 +0700

    add register and login gui

--- TheRedJournal/app/src/main/java/com/example/alphacr/theredjournal/LoginActivity.java
+++ TheRedJournal/app/src/main/java/com/example/alphacr/theredjournal/LoginActivity.java
@@ -1,21 +1,40 @@
 package com.example.alphacr.theredjournal;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class LoginActivity extends AppCompatActivity {
-
+    Button btnLogin;
+    EditText inputEMail;
+    EditText inputPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
+        inputEMail = (EditText) findViewById(R.id.eMail);
+        inputPassword = (EditText) findViewById(R.id.password);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
 
+    }
     public void onRegister (View view){
         startActivity(new Intent(this, Register.class
         ));
     }
-
 }
