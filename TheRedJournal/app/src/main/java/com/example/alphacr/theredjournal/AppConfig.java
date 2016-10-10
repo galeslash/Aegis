20ac8ce8afa8714008fd6e9efe0a46f9b4ddac8e
Isaac Raynald <Isaac Raynald>
2016-10-10 10:27:04 +0700

    fix commit

---
+++ TheRedJournal/app/src/main/java/com/example/alphacr/theredjournal/AppConfig.java
@@ -0,0 +1,12 @@
+package com.example.alphacr.theredjournal;
+
+/**
+ * Created by Ucer on 10/8/2016.
+ */
+
+public class AppConfig {
+    public static String URL_LOGIN =  "http://redjournal.hol.es/login.php";
+    public static String URL_REGISTER =  "http://redjournal.hol.es/register.php";
+    public static String URL_CONTACT_US = "http://redjournal.hol.es/contact_us.php";
+
+}
