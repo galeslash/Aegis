19efcaec838c53e1272866b949d0977295fdd9ad
alphacr <ricky.arkan@ui.ac.id>
2016-10-13 11:11:08 +0700

    Trying to combine with master 2

--- TheRedJournal/app/src/main/java/com/example/alphacr/theredjournal/HomePage.java
+++ TheRedJournal/app/src/main/java/com/example/alphacr/theredjournal/HomePage.java
@@ -1,5 +1,6 @@
 package com.example.alphacr.theredjournal;
 
+
 import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,10 +9,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import helper.SQLITEHandler;
+import helper.SessionManager;
 
 public class HomePage extends AppCompatActivity {
 
-
+    EditText contactForm;
+    private SQLITEHandler db;
+    private SessionManager session;
     TextView factBox;
 
     @Override
@@ -20,6 +32,7 @@ public class HomePage extends AppCompatActivity {
         Facts factHolder;
         Button button;
         Button guide;
+        Button btnLogOut;
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
@@ -44,5 +57,36 @@ public class HomePage extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "How to Donor is pressed", Toast.LENGTH_SHORT).show();
             }
         });
+
+        contactForm = (EditText) findViewById(R.id.contactUs);
+
+        btnLogOut = (Button) findViewById(R.id.btnLogOut);
+
+        db = new SQLITEHandler(getApplicationContext());
+
+        session = new SessionManager(getApplicationContext());
+
+        btnLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logOutUser();
+            }
+        });
+
+
+    }
+
+    public void contactUs(View view) {
+
+        startActivity(new Intent(this, Contact_Us.class));
+        finish();
+    }
+    private  void logOutUser(){
+    session.setLogin(false);
+    db.deleteUsers();
+
+    Intent intent = new Intent (HomePage.this, LoginActivity.class);
+    startActivity(intent);
+    finish();
     }
 }
