2f92ee597971ae43599a8c58f92e8f99ca02df26
alphacr <ricky.arkan@ui.ac.id>
2016-10-20 04:05:44 +0700

    Action Bar

--- TheRedJournal/app/src/main/java/com/example/alphacr/theredjournal/HomePage.java
+++ TheRedJournal/app/src/main/java/com/example/alphacr/theredjournal/HomePage.java
@@ -117,6 +117,30 @@ public class HomePage extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_logout){
+            return true;
+        } else if (id == R.id.action_contact_us) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
     private void logOutUser(){
         session.setLogin(false);
         db.deleteUsers();
