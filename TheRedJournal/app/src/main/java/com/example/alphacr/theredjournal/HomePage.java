705c62989d81895006e621aab3fda8e7af8692d7
alphacr <ricky.arkan@ui.ac.id>
2016-10-20 04:05:44 +0700

    Action Bar

--- TheRedJournal/app/src/main/java/com/example/alphacr/theredjournal/HomePage.java
+++ TheRedJournal/app/src/main/java/com/example/alphacr/theredjournal/HomePage.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.TextureView;
 import android.view.View;
 import android.widget.Button;
@@ -62,7 +64,8 @@ public class HomePage extends AppCompatActivity {
                 finish();
             }
         });
-        btnLogOut = (Button) findViewById(R.id.btnLogOut);
+        // Logout
+        btnLogOut = (Button) findViewById(R.id.action_logout);
         db = new SQLITEHandler(getApplicationContext());
         session = new SessionManager(getApplicationContext());
         btnLogOut.setOnClickListener(new View.OnClickListener() {
@@ -71,7 +74,8 @@ public class HomePage extends AppCompatActivity {
                 logOutUser();
             }
         });
-        contactUs = (Button) findViewById(R.id.contact);
+        // Contact Us
+        contactUs = (Button) findViewById(R.id.action_contact_us);
         contactUs.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -93,7 +97,31 @@ public class HomePage extends AppCompatActivity {
 
     }
 
-    private void logOutUser() {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_logout){
+            return true;
+        } else if (id == R.id.action_contact_us) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logOutUser(){
         session.setLogin(false);
         db.deleteUsers();
         Intent intent = new Intent(HomePage.this, LoginActivity.class);
