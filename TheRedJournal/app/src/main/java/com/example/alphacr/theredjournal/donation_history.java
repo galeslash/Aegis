7b83b4a9d2ebc4c1da64dfee696aa5fafc83c0f0
Revinaldy Emirza <revinaldy.emirza@ui.ac.id>
2016-11-07 14:34:37 +0700

    Donation history

---
+++ TheRedJournal/app/src/main/java/com/example/alphacr/theredjournal/donation_history.java
@@ -0,0 +1,31 @@
+package com.example.alphacr.theredjournal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+public class donation_history extends AppCompatActivity {
+    RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donation_history);
+
+        recyclerView= (RecyclerView) findViewById(R.id.recycler_view);
+        //menampilkan reyclerview yang ada pada file layout dengan id reycler view
+
+        ReyclerAdapter adapter=new ReyclerAdapter(this);
+        //membuat adapter baru untuk reyclerview
+        recyclerView.setAdapter(adapter);
+        //menset nilai dari adapter
+        recyclerView.setHasFixedSize(true);
+        //menset setukuran
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        //menset layoutmanager dan menampilkan daftar/list
+        //dalam bentuk linearlayoutmanager pada class saat ini
+
+
+    }
+}
